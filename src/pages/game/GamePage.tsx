70c202a6652d9875658ce153fef33b27d3ccd335
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2025-05-24 10:24:30 -0500

    feat: Implementación de Loading Screen

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,7 +1,20 @@
-import HeaderGame from "./components/HeaderGame"
-import LogicGame from "./components/LogicGame"
+import { useEffect, useState } from 'react';
+import HeaderGame from "./components/HeaderGame";
+import LogicGame from "./components/LogicGame";
+import LoadingScreen from './components/LoadingScreen';
 
 function GamePage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, [])
+
+  if (isLoading) {
+    return <LoadingScreen />
+  }
   return (
     <div className="flex flex-col w-full h-screen">
       <section className="h-30 w-full">
